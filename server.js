4237a4426165564466584b264fb5d4636c1cd7aa
teck7 <teckyap@hotmail.com.au>
2017-07-06 15:43:12 +1000

    Added Vehicle Model Schema

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var vehicle = require('./app/models/vehicle');
 
 // Configure app for bodyParser()
 // lets us grab data from the body of POST
