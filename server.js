bea4cbba20965d7e09a0b6ef3b4688cff63447c4
teck7 <teckyap@hotmail.com.au>
2017-07-06 15:37:36 +1000

    setup basic api route

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// Configure app for bodyParser()
+// lets us grab data from the body of POST
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Set up port for server to listen on
+var port = process.env.PORT || 3000;
+
+// Connect to DB
+mongoose.connect('mongodb://localhost:27017/node');
+
+// API Routes
+var router = express.Router();
+
+// Routes will all be prefixed with /api
+app.use('/api', router);
+
+// Test Route
+router.get('/', function(req, res) {
+  res.json({message: "Welcome to our API"});
+});
+
+// Fire up server
+app.listen(port);
+
+// Print friendly message to console
+console.log('Server listening on port ' + port);
