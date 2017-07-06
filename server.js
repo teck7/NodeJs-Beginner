f01d33a3c016d209a77cd19c1babd4a3d56adcd0
teck7 <teckyap@hotmail.com.au>
2017-07-06 16:17:31 +1000

    Create get and post router for Vehicle Model

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var vehicle = require('./app/models/vehicle');
+var Vehicle = require('./app/models/vehicle');
 
 // Configure app for bodyParser()
 // lets us grab data from the body of POST
@@ -21,10 +21,76 @@ var router = express.Router();
 // Routes will all be prefixed with /api
 app.use('/api', router);
 
+// Middleware :
+// can be very useful for doing validations
+// can log things from here or stop the
+// request from continuing in the event that
+// the request is not safe
+// middleware to use for all requests
+router.use(function(req, res, next) {
+  console.log('FYI....This is something currently going down.....');
+  next()
+});
+
 // Test Route
 router.get('/', function(req, res) {
   res.json({message: "Welcome to our API"});
 });
+
+
+router.route('/vehicles')
+  .post(function(req, res) {
+    var vehicle = new Vehicle(); // new instance of a vehicle
+    vehicle.make = req.body.make;
+    vehicle.model = req.body.model;
+    vehicle.color = req.body.color;
+
+    vehicle.save(function(err) {
+      if (err) {
+        res.send(err);
+      }
+      res.json({message: 'Vehicle was successfully manufactured'});
+    });
+  })
+
+  .get(function(req, res) {
+    Vehicle.find(function(err, vehicles) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(vehicles);
+    });
+  });
+
+router.router('/vehicle/:vehicle_id')
+  .get(function(req, res) {
+    Vehicle.findById(req.params.vehicle_id, function(err, vehicle) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(vehicles);
+    });
+  });
+
+router.route('/vehicle/make/:make')
+  .get(function(req, res) {
+    Vehicle.find({make:req.params.make}, function(err, vehicle) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(vehicle);
+    });
+  });
+
+router.route('/vehicle/color/:color')
+  .get(function(req, res) {
+    Vehicle.find({color:req.params.color}, function(err, vehicle) {
+      if (err) {
+        res.send(err)
+      }
+      res.json(vehicle);
+    });
+  });
 
 // Fire up server
 app.listen(port);
